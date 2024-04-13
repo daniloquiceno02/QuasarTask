b720d58cb7efdda71f4e5cfb8bf20442f6fe6368
daniloquiceno02 <daniloquiceno02@gmail.com>
2024-04-13 07:07:06 -0500

    Build V2

---
+++ dist/spa/assets/axios.4c778e91.js
@@ -0,0 +1,1 @@
+import{b as i}from"./index.f80d0101.js";import{a}from"./axios.37d53be1.js";const e=a.create({baseURL:"https://api.example.com"});var t=i(({app:o})=>{o.config.globalProperties.$axios=a,o.config.globalProperties.$api=e});export{e as api,t as default};
---
+++ dist/spa/assets/use-dark.19a4f461.js
@@ -0,0 +1,1 @@
+import{f as e}from"./index.f80d0101.js";const u={dark:{type:Boolean,default:null}};function o(a,r){return e(()=>a.dark===null?r.dark.isActive:a.dark)}export{u as a,o as u};
